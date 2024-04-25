42057a6e236b61e1771363cddf9a074f4c5365d5
waijinchai <wcha0106@student.monash.edu>
2024-04-25 09:06:20 +0800

    Add function to save uploaded file

--- OpenFace/waijin_extract_fau.py
+++ OpenFace/waijin_extract_fau.py
@@ -3,14 +3,26 @@ import subprocess
 import pandas as pd
 
 # set the directories
-input_dir = "C:/Users/User/Desktop/MonashDocuments/FIT3162/Repo/fit3162-mcs07/OpenFace/claire_testing_commandline/input_videos"   # directory of where the videos are
-output_dir = "C:/Users/User/Desktop/MonashDocuments/FIT3162/Repo/fit3162-mcs07/OpenFace/claire_testing_commandline/output"  # directory to output the csv file
+input_dir = "C:/Users/User/Desktop/MonashDocuments/FIT3162/Repo/fit3162-mcs07/OpenFace/input_videos"   # directory of where the videos are
+output_dir = "C:/Users/User/Desktop/MonashDocuments/FIT3162/Repo/fit3162-mcs07/OpenFace/output"  # directory to output the csv file
 of_dir = "C:/Users/User/Desktop/MonashDocuments/FIT3162/Code/OpenFace_2.2.0_win_x64/FeatureExtraction.exe"  # directory of where the FeatureExtraction.exe is at
 
-# run the script using the command line
-cmd_str = f"""for /F %i in ('dir /b "{input_dir}"') do "{of_dir}" -inroot "{input_dir}" -f %i  -aus -out_dir "{output_dir}" """
-subprocess.run(cmd_str, shell=True, text=True, stdout=True)
+def extract_fau(input_video) -> pd.DataFrame:
+    print(input_video)
+    cmd_str = f""" "{of_dir}" -f "{input_dir}/{input_video.name}" -out_dir "{output_dir}" -aus"""
+    subprocess.run(cmd_str, shell=True, text=True, stdout=True)
 
-# import the output csv
-df = pd.read_csv("C:/Users/User/Desktop/MonashDocuments/FIT3162/Repo/fit3162-mcs07/OpenFace/claire_testing_commandline/output/russell_test.csv")
-# print(df.sample(n=5))
+    input_vid_name = input_video.name[:-4]
+    df = pd.read_csv(f"C:/Users/User/Desktop/MonashDocuments/FIT3162/Repo/fit3162-mcs07/OpenFace/output/{input_vid_name}.csv")  
+
+    return df  
+
+if __name__ == "__main__":
+    # # run the script using the command line
+    cmd_str = f"""for /F %i in ('dir /b "{input_dir}"') do "{of_dir}" -inroot "{input_dir}" -f %i  -aus -out_dir "{output_dir}" """
+    subprocess.run(cmd_str, shell=True, text=True, stdout=True)
+
+    # import the output csv
+    df = pd.read_csv("C:/Users/User/Desktop/MonashDocuments/FIT3162/Repo/fit3162-mcs07/OpenFace/output/clairevid0.csv")
+    print(df.sample(n=5))
+    # pass
--- depression.py
+++ depression.py
@@ -1,13 +1,41 @@
+import os
 import streamlit as st
 import numpy as np
 import pandas as pd
+from OpenFace.waijin_extract_fau import extract_fau
 
 st.title("AI in Depression and Anxiety Understanding")
 
 uploaded_file = st.file_uploader("Choose a file", type=["mp4"])
 
+
+def save_uploaded_file(uploaded_file):
+    SAVE_PATH = "C:/Users/User/Desktop/MonashDocuments/FIT3162/Repo/fit3162-mcs07/OpenFace/input_videos/"
+    try:
+        with open(os.path.join(SAVE_PATH, uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.get_buffer())
+        return True
+    except Exception as e:
+        print(e)
+        return False
+    
+# if uploaded_file is not None:
+#     result = save_uploaded_file(uploaded_file)
+#     if result:
+#         st.success("File saved suffesscully!")
+#     else:
+#         st.error("Failed to save file!")
+
+
 st.subheader(f"Video: {uploaded_file.name[:-4]}")
 st.video(uploaded_file)
+
+extract_fau_state = st.text("Extracting FAUs...")
+
+df = extract_fau(uploaded_file)
+
+extract_fau_state.text("Extracting FAUs...Done!!")
+
 
 # DATE_COLUMN = 'date/time'
 # DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
